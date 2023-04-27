afdfc8e62147cb758c3bc7f1542826523a47a109
Marshall Carlson <124011504+yeeyeebrudder@users.noreply.github.com>
2023-04-27 17:54:01 -0500

    added timezone lunch

--- Ai.py
+++ Ai.py
@@ -1,30 +1,37 @@
 from datetime import datetime
-print('Hello! I am your AI dispatcher for today.\n')
+from datetime import timedelta
+from pytz import timezone
+import pytz
+
+
+def main():
+    print('Hello! I am your AI dispatcher for today.\n')
+    beginingPrompt()
+    
 def beginingPrompt():
     print('Please type a command:\n\"Run to _\",\n\"Complete\",\n\"Break\",\n\"Lunch\",\n\"End\",\n\"Help\",\nor \"rep\" if you need a human.')
-def getRep():
-    #notify rep
-    print('I\'m Fetching a dispatcher for you right now.')
-beginingPrompt()
 
+main()
 value = input()
 value = value.lower()
+currentTime = datetime.now(timezone('America/Chicago'))
 if 'run to' in value:
-    startRun
+    startRun()
 elif 'complete' in value or "10-24" in value:
     print('all done')
 elif 'thank you' in value:
     print('My pleasure')
 elif 'end' in value:
-    print('Goodbye! It was my pleasure to serve you today')
+    print('Goodbye! It was my pleasure to serve you today' )
 elif 'break' in value:
     print('Enjoy!')
 elif 'lunch' in value:
-    print('Enjoy! see you back at ')
+    lunch = currentTime+ timedelta(minutes=30)
+    print('See you back at '+ lunch.strftime("%H:%M") + ', Enjoy!' )
 elif 'help' in value:
     beginingPrompt()
     print('Would you like me to contact a dispatcher?')
-    if input().lower = 'yes':
+    if input().lower == 'yes':
         getRep()
 elif 'rep' in value:
     getRep()
@@ -32,3 +39,8 @@ else:
     print('I\'m sorry, I am not understanding your request.\n')
     beginingPrompt()
     
+def getRep():
+    #notify rep
+    print('I\'m Fetching a dispatcher for you right now.')
+def startRun():
+    print()
