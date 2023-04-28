b9881980ca60e3d217bd9a5867f611f4e1f66baa
Marshall Carlson <124011504+yeeyeebrudder@users.noreply.github.com>
2023-04-27 21:29:22 -0500

    organizational

--- Ai.py
+++ Ai.py
@@ -3,10 +3,19 @@ from datetime import timedelta
 from pytz import timezone
 import pytz
 
-courierType = None
-shift = -1
-runs = []
-done = False
+
+
+class Courier:
+    Name = None
+    courierType = None
+    shift = -1
+    runs = []
+    done = False
+
+    def __init__(self):
+        print('hello')
+        
+currentCourier = Courier()
 
 class Entry:
     destination = ''
@@ -19,14 +28,15 @@ class Entry:
     def complete(self):
         self.EndTime = datetime.now(timezone('America/Chicago'))
         print('Completing your ' + self.StartTime.strftime("%H:%M") + ' ' + self.destination + ' entry at ' + self.EndTime.strftime("%H:%M"))
-        
+
 def beginingPrompt():
     print('Hey there! Before we get started, can you tell me if you are on Lab or Pharmacy?')
-    courierType = input()
+    currentCourier.courierType = input()
     print('Perfect! now do you mind telling me which shift you are working today?')
-    shift = input()
-    print('Just to confirm, today you are a ' + str(courierType) + ' courier on ' + str(shift) + ' shift?')
+    currentCourier.shift = input()
+    print('Just to confirm, today you are a ' + str(currentCourier.courierType) + ' courier on ' + str(currentCourier.shift) + ' shift?')
     if 'yes' in input():
+
         print('You\'re good to go!\n')
         instructionsPrompt()
     else:
@@ -38,53 +48,61 @@ def instructionsPrompt():
     
 def startRun(destination):
     currentRun = Entry(destination)
-    runs.append(currentRun)
+    currentCourier.runs.append(currentRun)
     print('Logging an entry to ' + currentRun.destination + ' starting at ' + currentRun.StartTime.strftime("%H:%M"))
-
-def main():
-    print('Hello! I am your AI dispatcher for today.\n')
-    beginingPrompt()
-    
+def complete():
+    if len(currentCourier.runs) == 0:
+        print('try calling a run first.')
+    else:    
+        currentCourier.runs[-1].complete()
+def breakentry():
+    print('Enjoy!')
+    currentCourier.runs.append(Entry('break'))
+def lunch():
+    currentTime = datetime.now(timezone('America/Chicago'))
+    lunch = currentTime+ timedelta(minutes=30)
+    print('See you back at '+ lunch.strftime("%H:%M") + ', Enjoy!' )
+    currentCourier.runs.append(Entry('lunch'))
+def help():
+    instructionsPrompt()
+    print('Would you like me to contact a dispatcher?')
+    value = input()
+    if 'yes' in value.lower():
+        getRep()
+    else:
+        instructionsPrompt()
 def getRep():
     #notify rep
     print('I\'m fetching a dispatcher for you right now.')
-    
+
+def main():
+    print('Hello! I am your AI dispatcher for today.\n')
+    beginingPrompt()    
+    while not currentCourier.done:
+        value = input()
+        value = value.lower()
+        
+        if 'run to' in value:
+            destination = value.replace('run to ','')
+            startRun(destination)
+        elif 'complete' in value or "10-24" in value:
+            complete()
+        elif 'break' in value:
+            breakentry()
+        elif 'lunch' in value:
+            lunch()
+        elif 'help' in value:
+            help()
+        elif 'rep' in value:
+            getRep()
+        elif 'thank you' in value:
+            print('My pleasure!')
+        elif 'end' in value:
+            print('Goodbye! It was my pleasure to serve you today' )
+            currentCourier.done = True
+        else:
+            print('I\'m sorry, I am not understanding your request.\n')
+            instructionsPrompt()
+
 main()
     
-while not done:
-    value = input()
-    value = value.lower()
-    currentTime = datetime.now(timezone('America/Chicago'))
-    if 'run to' in value:
-        destination = value.replace('run to ','')
-        startRun(destination)
-    elif 'complete' in value or "10-24" in value:
-        if len(runs) == 0:
-            print('try calling a run first.')
-        else:    
-            runs[-1].complete()
-    elif 'thank you' in value:
-        print('My pleasure!')
-    elif 'end' in value:
-        print('Goodbye! It was my pleasure to serve you today' )
-        done = True
-    elif 'break' in value:
-        print('Enjoy!')
-        runs.append(Entry('break'))
-    elif 'lunch' in value:
-        lunch = currentTime+ timedelta(minutes=30)
-        print('See you back at '+ lunch.strftime("%H:%M") + ', Enjoy!' )
-        runs.append(Entry('lunch'))
-    elif 'help' in value:
-        instructionsPrompt()
-        print('Would you like me to contact a dispatcher?')
-        value = input()
-        if 'yes' in value.lower():
-            getRep()
-        else:
-            instructionsPrompt()
-    elif 'rep' in value:
-        getRep()
-    else:
-        print('I\'m sorry, I am not understanding your request.\n')
-        instructionsPrompt()
